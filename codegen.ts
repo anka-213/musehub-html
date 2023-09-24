fa7675c662f9a212c02e4e36dad675645250927d
Andreas Källberg <anka.213@gmail.com>
2023-09-24 14:53:54 +0200

    remove whitespace

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://muse-webservice.azurewebsites.net/graphql ",
+  schema: "https://muse-webservice.azurewebsites.net/graphql",
   documents: "src/**/*.tsx",
   generates: {
     "src/gql/": {

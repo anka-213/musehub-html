e87b794ce61ed6eed216b21fbef06809a0b9726a
Andreas Källberg <anka.213@gmail.com>
2023-09-24 13:27:44 +0200

    Add apollo demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+interface Locations {
+  id: string;
+  name: string;
+  description: string;
+  photo: string;
+}
+
+interface MyQuery {
+  locations: [Locations];
+}
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery<MyQuery>(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+  if (!data) return <p>Got no data</p>;
+
+  return (
+    <>
+      {data.locations.map(({ id, name, description, photo }) => (
+        <div key={id}>
+          <h3>{name}</h3>
+          <img
+            width="400"
+            height="250"
+            alt="location-reference"
+            src={`${photo}`}
+          />
+          <br />
+          <b>About this location:</b>
+          <p>{description}</p>
+          <br />
+        </div>
+      ))}
+    </>
+  );
+}
 
 function App() {
   return (
@@ -8,7 +59,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and do save to reload.
         </p>
         <a
           className="App-link"
@@ -18,6 +69,7 @@ function App() {
         >
           Learn React
         </a>
+        <DisplayLocations />
       </header>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
@@ -17,3 +24,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// import React from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+// import App from './App';
+
+// // Supported in React 18+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
